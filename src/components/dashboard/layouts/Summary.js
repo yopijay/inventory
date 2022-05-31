ad54bc4dcca2ed7763a4c1389b3735fb6b0a74e8
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2022-05-31 15:54:46 +0800

    Update Summary.js

--- src/components/dashboard/layouts/Summary.js
+++ src/components/dashboard/layouts/Summary.js
@@ -55,7 +55,7 @@ const Summary = () => {
                 bgcolor= "#1abc9c" display= "flex" flexDirection= "column" justifyContent= "flex-start" alignItems= "flex-start" borderRadius= "6px" padding= "20px 18px">
                     <Ctrl.Typography color= "#FFFFFF" text= "TOTAL ASSETS" style= {{ fontSize: '130%' }} />
                     <Box display= "flex" flexDirection= "row" width= "100%" justifyContent= "center" alignItems= "center">
-                        <Ctrl.Typography color= "#bdc3c7" text= { asset !== undefined ? asset[0].total : '0' } style= {{ fontSize: '450%' }} />
+                        <Ctrl.Typography color= "#bdc3c7" text= { asset !== undefined ? asset[0].total !== null ? asset[0].total : '0' : '0' } style= {{ fontSize: '450%' }} />
                     </Box>
                 </Box>
             </Grid>
