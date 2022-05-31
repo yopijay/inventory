15c62fec2b2feab9459593fdb5fb6b2a61bc0240
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2022-05-31 11:38:29 +0800

    Update index.js

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -12,7 +12,7 @@ const Index = () => {
 
     return (
         <Box padding= "10px 30px">
-            <Ctrl.Typography color= "#2c3e50" text= "DASHBOARD MO TO" style= {{ fontSize: '200%', fontWeight: 'bold' }} />
+            <Ctrl.Typography color= "#2c3e50" text= "DASHBOARD" style= {{ fontSize: '200%', fontWeight: 'bold' }} />
             <Box margin= "20px 0 0 0"><Summary /></Box>
         </Box>
     );
