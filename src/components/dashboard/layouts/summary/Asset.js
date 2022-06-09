24097a0f7821b17ea5f937cb72ec0bf6ff19c056
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2022-06-09 10:02:54 +0800

    Assets
    - Update assets automatically after an asset assigned to user

--- src/components/dashboard/layouts/summary/Asset.js
+++ src/components/dashboard/layouts/summary/Asset.js
@@ -17,9 +17,12 @@ const Asset = () => {
         sum(setCountAssetAssigned, 'assigned_asset', 'quantity');
     }, []);
 
-    let unAssigned = asset !== undefined && assetAssigned !== undefined ? 
-                                asset[0].total !== null && assetAssigned[0].total !== null ? 
-                                    (parseInt(asset[0].total) - parseInt(assetAssigned[0].total)) : '0' : '0';
+    let total = asset !== undefined && assetAssigned !== undefined ? 
+                        asset[0].total !== null && assetAssigned[0].total !== null ? 
+                        (parseInt(asset[0].total) + parseInt(assetAssigned[0].total)) : '0' : '0';
+    let unAssigned = assetAssigned !== undefined ? 
+                                assetAssigned[0].total !== null ? 
+                                    total - parseInt(assetAssigned[0].total) : '0' : '0';
 
     return (
         <Grid container direction= "column" justifyContent= "flex-start" alignItems= "stretch" spacing= { 2 }>
@@ -28,7 +31,7 @@ const Asset = () => {
                     bgcolor= "#1dd1a1" display= "flex" flexDirection= "column" justifyContent= "flex-start" alignItems= "flex-start" borderRadius= "6px" padding= "20px 18px">
                     <Ctrl.Typography color= "#FFFFFF" text= "TOTAL ASSETS" style= {{ fontSize: '130%' }} />
                     <Box display= "flex" flexDirection= "row" width= "100%" justifyContent= "center" alignItems= "center">
-                        <Ctrl.Typography color= "#bdc3c7" text= { asset !== undefined ? asset[0].total !== null ? asset[0].total : '0' : '0' } style= {{ fontSize: '450%' }} />
+                        <Ctrl.Typography color= "#bdc3c7" text= { total } style= {{ fontSize: '450%' }} />
                     </Box>
                 </Box>
             </Grid>
