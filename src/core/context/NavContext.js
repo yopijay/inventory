5fa68c488f61502458b8fdf8358d9ebf6f28c118
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2022-06-29 10:42:22 +0800

    Update NavContext.js

--- src/core/context/NavContext.js
+++ src/core/context/NavContext.js
@@ -6,9 +6,11 @@ export const NavContext = createContext();
 export const NavProvider = (props) => {
     const [ isActive, setIsActive ] = useState(localStorage.getItem('nav') !== null ? localStorage.getItem('nav') : 'Home');
     const [ expand, setExpand ] = useState(localStorage.getItem('nav') !== null ? 
-                                                                    localStorage.getItem('nav') === 'Categories' || localStorage.getItem('nav') === 'Brands' ||
+                                                                    localStorage.getItem('nav') === 'Categories' || localStorage.getItem('nav') === 'Brands' || localStorage.getItem('nav') === 'Customer' ||
                                                                     localStorage.getItem('nav') === 'Users' || localStorage.getItem('nav') === 'Assets'? 'Maintenance' :
-                                                                    localStorage.getItem('nav') === 'Assign Assets' ? 'Issuance' : '' : '');
+                                                                    localStorage.getItem('nav') === 'Assign Assets' || localStorage.getItem('nav') === 'Test Report' ? 'Issuance' :
+                                                                    localStorage.getItem('nav') === 'Categories Report' || localStorage.getItem('nav') === 'Brands Report' ||
+                                                                    localStorage.getItem('nav') === 'Users Report' || localStorage.getItem('nav') === 'Assets Report' ? 'Reports' : '' : '');
 
     return (
         <NavContext.Provider value= {[ isActive, setIsActive, expand, setExpand ]}>
